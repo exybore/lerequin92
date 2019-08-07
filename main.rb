363c07c5c270b52aae2c0ace95b96d6b0d1ae01e
Exybore <exybore@becauseofprog.fr>
2019-08-07 18:50:50 +0200

    feat: add list command
    Added a command to list all available sounds.
    In order to make this, modified the bot type to "CommandBot" and
    used the build-in command library.

--- main.rb
+++ main.rb
@@ -3,9 +3,11 @@ require 'json'
 
 config = JSON.parse(File.read(File.dirname(__FILE__) + '/config.json'))
 
-bot = Discordrb::Bot.new(token: config['token'],
+bot = Discordrb::Commands::CommandBot.new(token: config['token'],
                          log_mode: config['debug'] ? :debug : :normal,
-                         client_id: config['client_id'],)
+                         client_id: config['client_id'],
+                         prefix: config['prefix'],
+                         help_command: false)
 
 bot.ready do
   bot.voice_connect(272641655709106176)
@@ -14,10 +16,26 @@ bot.ready do
 end
 
 bot.message(in: 272639973352538123, starts_with: '92:') do |event|
-  voice = event.voice
   sound_name = event.content.sub(/92:/, '')
+  next if sound_name == 'list'
+  
+  voice = event.voice
   event.message.delete
   voice.play_file(Dir.pwd + "/sounds/#{sound_name}.mp3")
+end
+
+bot.command(:list,
+            description: 'Lister tous les sons disponibles',
+            channels: [272639973352538123]) do |event|
+  sounds = ''
+  Dir.each_child('sounds') do |sound|
+    sounds += "• #{sound.sub(/\.mp3/, '')}\n"
+  end
+  event.send_embed('', Discordrb::Webhooks::Embed.new(
+    color: config['color'],
+    title: 'Liste des sons',
+    description: sounds
+  ))
 end
 
 bot.run
