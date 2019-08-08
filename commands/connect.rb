38bddf7a1fceacb611f99aee07cde784c49fef28
Exybore <exybore@becauseofprog.fr>
2019-08-08 20:25:13 +0200

    feat: add error handling
    Added a full and clean error handling, so no errors are produced by the
    code and the user can know everything.

--- commands/connect.rb
+++ commands/connect.rb
@@ -1,13 +1,13 @@
 module LeRequin92
   $bot.command(:connect,
-              description: 'Se connecter à un salon vocal') do |event|
+               description: 'Se connecter à un salon vocal') do |event|
     channel = event.author.voice_channel
     event.message.delete
-    unless channel
+    if channel
+      $bot.voice_connect(channel)
+    else
       event.send_temporary_message ':x: Vous devez être connecté à un salon vocal.', 2
-      next
     end
-    $bot.voice_connect(channel)
     return
   end
 end
--- commands/list.rb
+++ commands/list.rb
@@ -1,15 +1,20 @@
 module LeRequin92
   $bot.command(:list,
-              description: 'Lister tous les sons disponibles',
-              channels: [272639973352538123]) do |event|
+               description: 'Lister tous les sons disponibles',
+               channels: [272639973352538123]) do |event|
     sounds = ''
     ::Dir.each_child(CONFIG['sounds_dir']) do |sound|
       sounds += "• #{sound.sub(/\.mp3/, '')}\n"
     end
-    event.send_embed('', ::Discordrb::Webhooks::Embed.new(
-      color: CONFIG['color'],
-      title: 'Liste des sons',
-      description: sounds
-    ))
+
+    if sounds == ''
+      event.send_message ':x: Aucun son n\'est disponible. Contactez l\'administrateur du robot pour plus d\'informations.'
+    else
+      event.send_embed '', ::Discordrb::Webhooks::Embed.new(
+        color: CONFIG['color'],
+        title: 'Liste des sons',
+        description: sounds
+      )
+    end
   end
 end
